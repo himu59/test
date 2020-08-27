e109668b1a9c0527ca436fcf0c4ba46669a080da
Himanshu Negi <negehimanshu9756@gmail.com>
2020-08-27 19:05:41 +0530

    Updates

---
+++ precache-manifest.5fd9014bef1f42ee81b8acbf847fea6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "733976e234e4b5dfc554",
+    "url": "/test/static/css/main.b14a9b17.chunk.css"
+  },
+  {
+    "revision": "733976e234e4b5dfc554",
+    "url": "/test/static/js/main.f0ed15ea.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "1868546b76755d13629b",
+    "url": "/test/static/js/2.cdefec29.chunk.js"
+  },
+  {
+    "revision": "7c82d12ce92e17e6d74cdce3b121c90b",
+    "url": "/test/index.html"
+  }
+];
